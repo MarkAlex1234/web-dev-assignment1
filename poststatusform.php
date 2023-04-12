dc15412796045ca15d0690d2a4a15a5d7dfd6143
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-12 13:24:34 +1200

    saving progress

--- poststatusform.php
+++ poststatusform.php
@@ -10,18 +10,21 @@
 <body>
     <h1>Status Posting System</h1>
     <form action="poststatusprocess.php" method="post">
-        <label>Status Code: <input type="text" name="statuscode"></label>
+        <label>Status Code: <input type="text" name="statuscode" required="true"></label>
 	<br>
         <label>Status: <input type="text" name="status"></label>
 	<br>
-    <label>Share:</label>
-    <radioLabel><input type="radio" id="publicRadioOption" name="shareOption" value="Public"> Public</radioLabel>
-    <radioLabel><input type="radio" id="friendsRadioOption" name="shareOption" value="Friends"> Friends</radioLabel>
-    <radioLabel><input type="radio" id="onlyMeRadioOption" name="shareOption" value="Only Me"> Only Me</radioLabel>
+        <label>Share:</label>
+        <radioLabel><input type="radio" id="publicRadioOption" name="shareOption" value="Public"> Public</radioLabel>
+        <radioLabel><input type="radio" id="friendsRadioOption" name="shareOption" value="Friends"> Friends</radioLabel>
+        <radioLabel><input type="radio" id="onlyMeRadioOption" name="shareOption" value="Only Me"> Only Me</radioLabel>
 	<br>
-        <label>Date: <input type="date" name="date"></label>
+	<label>Date:</label>
+	<input type="text" name="date" value="<?php echo date('d/m/Y'); ?>">
 	<br>
-        <label>Permission: <input type="text" name="permission"></label>
+        <label>Permission:</label>
+        <checkboxLabel><input type="checkbox" id="likePermission" name="likePermission" value="Like"> Allow Like</checkboxLabel>        <checkboxLabel><input type="checkbox" id="commentPermission" name="commentPermission" value="Comment"> Allow Comments</checkboxLabel>
+        <checkboxLabel><input type="checkbox" id="sharePermission" name="sharePermission" value="Share"> Allow share</checkboxLabel>
 	<br>
         <button type="Post">Submit</button>
     </form>
