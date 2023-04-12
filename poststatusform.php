009df0d5fac6fb22523c8adda5796bd32d006ba6
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-12 19:08:47 +1200

    Update poststatusform.php

--- poststatusform.php
+++ poststatusform.php
@@ -10,22 +10,22 @@
 <body>
     <h1>Status Posting System</h1>
     <form action="poststatusprocess.php" method="post">
-        <label>Status Code: <input type="text" name="statuscode" required="true"></label>
+        <label>Status Code: <input type="text" name="statuscode" required></label>
 	<br>
-        <label>Status: <input type="text" name="status"></label>
+        <label>Status: <input type="text" name="status" required></label>
 	<br>
         <label>Share:</label>
-        <radioLabel><input type="radio" id="publicRadioOption" name="shareOption"> Public</radioLabel>
-        <radioLabel><input type="radio" id="friendsRadioOption" name="shareOption"> Friends</radioLabel>
-        <radioLabel><input type="radio" id="onlyMeRadioOption" name="shareOption"> Only Me</radioLabel>
+        <radioLabel><input type="radio" id="publicRadioOption" name="shareOption" value="Public" checked> Public</radioLabel>
+        <radioLabel><input type="radio" id="friendsRadioOption" name="shareOption" value="Friends"> Friends</radioLabel>
+        <radioLabel><input type="radio" id="onlyMeRadioOption" name="shareOption" value="Only Me"> Only Me</radioLabel>
 	<br>
-	    <label>Date:</label>
-	    <input type="text" name="date" value="<?php echo date('d/m/Y'); ?>">
+	<label>Date:</label>
+	<input type="text" name="date" value="<?php echo date('d/m/Y'); ?>" required>
 	<br>
         <label>Permission:</label>
-        <checkboxLabel><input type="checkbox" id="likePermission" name="likePermission" value="Like"> Allow Like</checkboxLabel>        
-        <checkboxLabel><input type="checkbox" id="commentPermission" name="commentPermission" value="Comment"> Allow Comments</checkboxLabel>
-        <checkboxLabel><input type="checkbox" id="sharePermission" name="sharePermission" value="Share"> Allow share</checkboxLabel>
+        <checkboxLabel><input type="checkbox" id="likePermission" name="likePermission"> Allow Like</checkboxLabel>        
+        <checkboxLabel><input type="checkbox" id="commentPermission" name="commentPermission"> Allow Comments</checkboxLabel>
+        <checkboxLabel><input type="checkbox" id="sharePermission" name="sharePermission"> Allow share</checkboxLabel>
 	<br>
         <button type="Post">Submit</button>
     </form>
