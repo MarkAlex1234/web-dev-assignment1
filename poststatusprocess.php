18b229a4d6f1b39c783729246700821710bb5dcb
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-12 19:08:26 +1200

    Update poststatusprocess.php

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -4,6 +4,9 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>Assignment 1 - Web Dev 2023</title>
 </head>
+<div class="content">
+<!--div class containing everything on the page except the
+header and footer--> 
 <body>
     <h1>Post Status Process</h1>
     <?php
@@ -12,9 +15,12 @@
 	$status = $_POST["status"];
 	$shareOption = $_POST["shareOption"];
 	$date = $_POST["date"];
-	$likePermission= $_POST["likePermission"];
-    	$commentPermission= $_POST["commentPermission"];
-    	$sharePermission= $_POST["sharePermission"];
+	$likePermission = $_POST["likePermission"];
+    	$commentPermission = $_POST["commentPermission"];
+    	$sharePermission = $_POST["sharePermission"];
+
+	$statusCodePattern = "/^S[0-9]{4}$/";
+	$statusPattern = "/^[a-zA-Z0-9\s\,\.\!\?]+$/";
 
         if($statuscode && $status && $shareOption && $date ){
         	require_once ("../../conf/settings.php"); 
@@ -31,9 +37,23 @@
 		} 
 
 		$sql_tble = "assignment_one";
- 		$timestamp = strtotime($date);
-    		$formattedDate = date("d/m/Y", $timestamp);
+		
+		// Checks if the status code is in the correct format
+		if (!preg_match("/^S[0-9]{4}$/", $statuscode)) {
+   			echo "Wrong format! The status code must start with an \"S\" followed by four digits, like \"S0001\" <br>";
+			echo '<a href="index.html"> Return to Homepage </a> <br>';
+			echo '<a href="poststatusform.php"> Post a new Status </a>';
+			return;
+		}
 
+		// Checks if the status is in the correct format
+		if (trim($status) === '' || empty($status) || !preg_match("/^[a-zA-Z0-9\s\,\.\!\?]+$/", $status)) {
+    			echo "Your status is in a wrong format! The status can only contain alphanumericals and spaces, comma, period, exclamation point and question mark and cannot be blank! <br>";
+			echo '<a href="index.html"> Return to Homepage </a> <br>';
+			echo '<a href="poststatusform.php"> Post a new Status </a>';
+			return;
+		}  
+ 	
 		// Validates date using checkdate function after removing the "/"
 		// index 0 = Day 
 		// index 1 = Month
@@ -42,9 +62,40 @@
 		
 
             	if (!checkdate($dateParts[1], $dateParts[0], $dateParts[2])) {
-                	echo "<p>Date is not valid: ", $date, "</p>";
+                	echo "<p>Date is not valid: ", $date, "</p> <br>";
+			echo '<a href="index.html"> Return to Homepage </a> <br>';
+			echo '<a href="poststatusform.php"> Post a new Status </a>';
                 	return;
             	}
+
+		// Sets date as a date from input type text to save in the database
+		try {
+			$dateObj = new DateTime($date);
+			$formattedDate = $dateObj->format('Y-m-d'); // The database date format is Year, Month, Day so we need to use that format here. 
+		} catch (Exception $e) {
+			echo "Invalid date format: " . $e->getMessage();
+			return;
+		}
+
+		// Sets permissions as an int to save in the database:  
+		if(!$likePermission){
+                    $likePermission = 0;
+                } else {
+                    $likePermission = 1;
+                }
+               
+		if(!$commentPermission){
+                    $commentPermission = 0;
+                } else {
+                    $commentPermission = 1;
+                }
+               
+		if(!$sharePermission){
+                    $sharePermission = 0;
+                } else {
+                    $sharePermission = 1;
+                }
+                
 		
 		// Create table if it does not exist
 		$sql = "CREATE TABLE IF NOT EXISTS $sql_tble (
@@ -71,7 +122,7 @@
 		$query = "INSERT INTO $sql_tble"
 			 ."(status_code, status_description, share, status_date, allow_like, allow_comment, allow_share)"
 			. " VALUES"
-			."('$statuscode','$status','$shareOption', '$formattedDate', '$likePermission', '$commentPermission', '$sharePermission')";
+			."('$statuscode','$status','$shareOption', '$formattedDate', $likePermission, $commentPermission, $sharePermission)";
 
 		// executes the query
 		$result = mysqli_query($conn, $query);
@@ -82,9 +133,9 @@
 			echo "<p>Error message: " . mysqli_error($conn) . "</p>";
 		} else {
 			// display an operation successful message
-			echo "<p>Success! " . $statuscode . " added! </p>";
-			echo "<p>". $formattedDate . "</p>";
-			echo "Created date is " . date("d/m/Y", $formattedDate);
+			echo "<p>Success! Status: " . $statuscode . " has been posted! </p>";
+			echo '<a href="index.html"> Return to Homepage </a> <br>';
+			echo '<a href="poststatusform.php"> Post a new Status </a>';
 		} 
 
 		// close the database connection
@@ -94,4 +145,5 @@
         }
     ?>
 </body>
+</div>
 </html>
