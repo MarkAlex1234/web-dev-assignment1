e758f0d94cf89956fa979b212769d02613f93d37
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-12 17:47:17 +1200

    Update poststatusprocess.php

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -8,13 +8,13 @@
     <h1>Post Status Process</h1>
     <?php
     
-    $statuscode = $_POST["statuscode"];
+    	$statuscode = $_POST["statuscode"];
 	$status = $_POST["status"];
 	$shareOption = $_POST["shareOption"];
 	$date = $_POST["date"];
 	$likePermission= $_POST["likePermission"];
-    $commentPermission= $_POST["commentPermission"];
-    $sharePermission= $_POST["sharePermission"];
+    	$commentPermission= $_POST["commentPermission"];
+    	$sharePermission= $_POST["sharePermission"];
 
         if($statuscode && $status && $shareOption && $date ){
         	require_once ("../../conf/settings.php"); 
@@ -31,6 +31,20 @@
 		} 
 
 		$sql_tble = "assignment_one";
+ 		$timestamp = strtotime($date);
+    		$formattedDate = date("d/m/Y", $timestamp);
+
+		// Validates date using checkdate function after removing the "/"
+		// index 0 = Day 
+		// index 1 = Month
+		// index 2 = Year
+            	$dateParts = explode('/', $date);
+		
+
+            	if (!checkdate($dateParts[1], $dateParts[0], $dateParts[2])) {
+                	echo "<p>Date is not valid: ", $date, "</p>";
+                	return;
+            	}
 		
 		// Create table if it does not exist
 		$sql = "CREATE TABLE IF NOT EXISTS $sql_tble (
@@ -45,27 +59,32 @@
             
 		// Executes the query
 		$result = mysqli_query($conn, $sql);
+
 		// checks if the execution was successful
 		if(!$result) {
 			echo "<p>Something is wrong with: ", $sql, "</p>";
+			echo "<p>Error message: " . mysqli_error($conn) . "</p>";
 			return;
 		}
-
-
+		
 		// Set up the SQL command to add the data into the table
 		$query = "INSERT INTO $sql_tble"
 			 ."(status_code, status_description, share, status_date, allow_like, allow_comment, allow_share)"
 			. " VALUES"
-			."('$statuscode','$status','$shareOption', '$date', '$likePermission', '$commentPermission', '$sharePermission')";
+			."('$statuscode','$status','$shareOption', '$formattedDate', '$likePermission', '$commentPermission', '$sharePermission')";
 
 		// executes the query
 		$result = mysqli_query($conn, $query);
+
 		// checks if the execution was successful
 		if(!$result) {
 			echo "<p>Something is wrong with: ", $query, "</p>";
+			echo "<p>Error message: " . mysqli_error($conn) . "</p>";
 		} else {
 			// display an operation successful message
-			echo "<p>Success! - Status -> " . $statuscode . " added! </p>";
+			echo "<p>Success! " . $statuscode . " added! </p>";
+			echo "<p>". $formattedDate . "</p>";
+			echo "Created date is " . date("d/m/Y", $formattedDate);
 		} 
 
 		// close the database connection
