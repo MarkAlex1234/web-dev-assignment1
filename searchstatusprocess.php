bdf88bee2db1054c78869187053493acb5ab7cba
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-14 11:02:38 +1200

    add search status form and process

---
+++ searchstatusprocess.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Assignment 1 - Web Dev 2023</title>
+  </head>
+    <div class="content">
+        <!--div class containing everything on the page except the header and
+        footer-->
+        <body>
+            <h1>Status Information</h1>
+        <?php 
+        ?>
+        <a href="searchstatusform.html">Search for another status</a>
+        <a href="index.html">Return to Home Page</a>
+        </body>
+    </div>
+</html>
