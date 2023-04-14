88651fa6d6edb3059751b4dc020916cd534a068b
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-14 13:46:06 +1200

    Update searchstatusprocess.php

--- searchstatusprocess.php
+++ searchstatusprocess.php
@@ -9,7 +9,7 @@
       <?php 
         $search = $_GET["Search"];
 
-	// If the string is empty or if only spaces are sent reject the search
+	// If the search is empty or if only spaces are sent reject the search
        	if(!(trim($search ) === '' || empty($search))){
            require_once ("../../conf/settings.php"); 
 	
@@ -29,9 +29,22 @@
 	    $result = mysqli_query($conn, $sql);
             if(!$result){
                 echo 'No status found in the system. Please go to the post status page to post one. <br>';
-                echo '<a href=poststatusform.php>Post a new Status<a>';
+                echo '<a href=poststatusform.php>Post a new Status<a> <br>';
+		echo '<a href=index.html>Return to Home Page<a>';
                 return;
             }
+
+	    $sql = "SELECT * FROM ".$sql_tble." WHERE status_description LIKE '%".$search."%'";
+	    // Executes the query to find status descriptions like the search sent
+	    $result = mysqli_query($conn, $sql);
+	    if(mysqli_num_rows($result) == 0){
+                echo 'Status not found. Please try a different keyword. <br> <br>';
+                echo '<a href=searchstatusform.html>Search for another status<a> <br>';
+		echo '<a href=index.html>Return to Home Page<a>';
+                return;
+            }
+
+		
 
         } else {
             echo 'The search string is empty. Please enter a keyword to search.';
