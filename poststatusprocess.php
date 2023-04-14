b8419728493aa7cf101b07cc2d9739d9474846ca
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-14 15:59:19 +1200

    indent

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -11,13 +11,13 @@ header and footer-->
     <h1>Post Status Process</h1>
     <?php
     
-    	$statuscode = $_POST["statuscode"];
+    $statuscode = $_POST["statuscode"];
 	$status = $_POST["status"];
 	$shareOption = $_POST["shareOption"];
 	$date = $_POST["date"];
 	$likePermission = $_POST["likePermission"];
-    	$commentPermission = $_POST["commentPermission"];
-    	$sharePermission = $_POST["sharePermission"];
+    $commentPermission = $_POST["commentPermission"];
+    $sharePermission = $_POST["sharePermission"];
 
 	$statusCodePattern = "/^S[0-9]{4}$/";
 	$statusPattern = "/^[a-zA-Z0-9\s\,\.\!\?]+$/";
@@ -25,82 +25,79 @@ header and footer-->
         if($statuscode && $status && $shareOption && $date ){
         	require_once ("../../conf/settings.php"); 
 	
-		$conn = mysqli_connect($host,
-			$user,
-			$pswd,
-			$dbnm);
+			$conn = mysqli_connect($host, $user, $pswd, $dbnm);
 
- 		// Checks if connection is successful
-		if (!$conn) {
-			// Displays an error message
+ 			// Checks if connection is successful
+			if (!$conn) {
+				// Displays an error message
     			die("Connection failed: " . mysqli_connect_error());
-		} 
+			} 
 
-		$sql_tble = "assignment_one";
+			$sql_tble = "assignment_one";
 		
-		// Checks if the status code is in the correct format
-		if (!preg_match("/^S[0-9]{4}$/", $statuscode)) {
-   			echo "Wrong format! The status code must start with an \"S\" followed by four digits, like \"S0001\" <br>";
-			echo '<a href="index.html"> Return to Homepage </a> <br>';
-			echo '<a href="poststatusform.php"> Post a new Status </a>';
-			return;
-		}
+			// Checks if the status code is in the correct format
+			if (!preg_match("/^S[0-9]{4}$/", $statuscode)) {
+   				echo "Wrong format! The status code must start with an \"S\" followed by four digits, like \"S0001\" <br>";
+				echo '<a href="index.html"> Return to Homepage </a> <br>';
+				echo '<a href="poststatusform.php"> Post a new Status </a>';
+				return;
+			}
 
-		// Checks if the status is in the correct format
-		if (trim($status) === '' || empty($status) || !preg_match("/^[a-zA-Z0-9\s\,\.\!\?]+$/", $status)) {
+			// Checks if the status is in the correct format
+			if (trim($status) === '' || empty($status) || !preg_match("/^[a-zA-Z0-9\s\,\.\!\?]+$/", $status)) {
     			echo "Your status is in a wrong format! The status can only contain alphanumericals and spaces, comma, period, exclamation point and question mark and cannot be blank! <br>";
-			echo '<a href="index.html"> Return to Homepage </a> <br>';
-			echo '<a href="poststatusform.php"> Post a new Status </a>';
-			return;
-		}  
+				echo '<a href="index.html"> Return to Homepage </a> <br>';
+				echo '<a href="poststatusform.php"> Post a new Status </a>';
+				return;
+			}  
  	
-		// Validates date using checkdate function after removing the "/"
-		// index 0 = Day 
-		// index 1 = Month
-		// index 2 = Year
-            	$dateParts = explode('/', $date);
+			// Validates date using checkdate function after removing the "/"
+			// index 0 = Day 
+			// index 1 = Month
+			// index 2 = Year
+            $dateParts = explode('/', $date);
 		
 
-            	if (!checkdate($dateParts[1], $dateParts[0], $dateParts[2])) {
-                	echo "<p>Date is not valid: ", $date, "</p> <br>";
-			echo '<a href="index.html"> Return to Homepage </a> <br>';
-			echo '<a href="poststatusform.php"> Post a new Status </a>';
-                	return;
-            	}
+            if (!checkdate($dateParts[1], $dateParts[0], $dateParts[2])) {
+                echo "<p>Date is not valid: ", $date, "</p> <br>";
+				echo '<a href="index.html"> Return to Homepage </a> <br>';
+				echo '<a href="poststatusform.php"> Post a new Status </a>';
+                return;
+            }
 
-		// Sets date as a date from input type text to save in the database
-		try {
-			$dateObj = DateTime::createFromFormat('d/m/Y', $date);
-			$formattedDate = $dateObj->format('Y-m-d'); // The database date format is Year, Month, Day so we need to use that format here. 
-		} catch (Exception $e) {
-			echo "Invalid date format: " . $e->getMessage();
-			return;
-		}
+			// Sets date as a date from input type text to save in the database
+			try {
+				$dateObj = DateTime::createFromFormat('d/m/Y', $date);
+				$formattedDate = $dateObj->format('Y-m-d'); // The database date format is Year, Month, Day so we need to use that format here. 
+			} catch (Exception $e) {
+				echo "Invalid date format: " . $e->getMessage();
+				return;
+			}
 
-		// Sets permissions as an int to save in the database:  
-		if(!$likePermission){
-                    $likePermission = 0;
+			// Sets permissions as an int to save in the database:  
+			if(!$likePermission){
+                $likePermission = 0;
                 } else {
-                    $likePermission = 1;
+                $likePermission = 1;
                 }
                
-		if(!$commentPermission){
-                    $commentPermission = 0;
+			if(!$commentPermission){
+                $commentPermission = 0;
                 } else {
-                    $commentPermission = 1;
+                $commentPermission = 1;
                 }
                
-		if(!$sharePermission){
-                    $sharePermission = 0;
+			if(!$sharePermission){
+                $sharePermission = 0;
                 } else {
-                    $sharePermission = 1;
+                $sharePermission = 1;
                 }
                 
 		
-		// Create table if it does not exist
-		$sql = "CREATE TABLE IF NOT EXISTS $sql_tble (
-    		status_code VARCHAR(5) PRIMARY KEY,
-    		status_description TEXT NOT NULL,
+			// Create table if it does not exist
+			$sql = "CREATE TABLE IF NOT EXISTS $sql_tble (
+    			status_code VARCHAR(5) PRIMARY KEY,
+    			status_description TEXT NOT NULL,
             	share VARCHAR(10) NOT NULL,
             	status_date DATE NOT NULL,
             	allow_like TINYINT(1) NOT NULL DEFAULT 0,
@@ -108,38 +105,38 @@ header and footer-->
             	allow_share TINYINT(1) NOT NULL DEFAULT 0
             	)";
             
-		// Executes the query
-		$result = mysqli_query($conn, $sql);
+			// Executes the query
+			$result = mysqli_query($conn, $sql);
 
-		// checks if the execution was successful
-		if(!$result) {
-			echo "<p>Something is wrong with: ", $sql, "</p>";
-			echo "<p>Error message: " . mysqli_error($conn) . "</p>";
-			return;
-		}
+			// checks if the execution was successful
+			if(!$result) {
+				echo "<p>Something is wrong with: ", $sql, "</p>";
+				echo "<p>Error message: " . mysqli_error($conn) . "</p>";
+				return;
+			}
 		
-		// Set up the SQL command to add the data into the table
-		$query = "INSERT INTO $sql_tble"
-			 ."(status_code, status_description, share, status_date, allow_like, allow_comment, allow_share)"
-			. " VALUES"
-			."('$statuscode','$status','$shareOption', '$formattedDate', $likePermission, $commentPermission, $sharePermission)";
+			// Set up the SQL command to add the data into the table
+			$query = "INSERT INTO $sql_tble"
+			 	."(status_code, status_description, share, status_date, allow_like, allow_comment, allow_share)"
+				. " VALUES"
+				."('$statuscode','$status','$shareOption', '$formattedDate', $likePermission, $commentPermission, $sharePermission)";
 
-		// executes the query
-		$result = mysqli_query($conn, $query);
+			// executes the query
+			$result = mysqli_query($conn, $query);
 
-		// checks if the execution was successful
-		if(!$result) {
-			echo "<p>Something is wrong with: ", $query, "</p>";
-			echo "<p>Error message: " . mysqli_error($conn) . "</p>";
-		} else {
-			// display an operation successful message
-			echo "<p>Success! Status: " . $statuscode . " has been posted! </p>";
-			echo '<a href="index.html"> Return to Homepage </a> <br>';
-			echo '<a href="poststatusform.php"> Post a new Status </a>';
-		} 
+			// checks if the execution was successful
+			if(!$result) {
+				echo "<p>Something is wrong with: ", $query, "</p>";
+				echo "<p>Error message: " . mysqli_error($conn) . "</p>";
+			} else {
+				// display an operation successful message
+				echo "<p>Success! Status: " . $statuscode . " has been posted! </p>";
+				echo '<a href="index.html"> Return to Homepage </a> <br>';
+				echo '<a href="poststatusform.php"> Post a new Status </a>';
+			} 
 
-		// close the database connection
-		mysqli_close($conn);
+			// close the database connection
+			mysqli_close($conn);
 	} else {
             echo "<p> Please enter a value for all fields in the input form.";
         }
--- searchstatusprocess.php
+++ searchstatusprocess.php
@@ -10,30 +10,30 @@
       <?php 
         $search = $_GET["Search"];
 
-	// If the search is empty or if only spaces are sent reject the search
+		// If the search is empty or if only spaces are sent reject the search
        	if(!(trim($search ) === '' || empty($search))){
            require_once ("../../conf/settings.php"); 
 	
-	   $conn = mysqli_connect($host, $user,$pswd,$dbnm);
+	   	$conn = mysqli_connect($host, $user,$pswd,$dbnm);
 
-            // Checks if connection is successful
+        // Checks if connection is successful
 	    if (!$conn) {
-		// Displays an error message
+			// Displays an error message
     		die("Connection failed: " . mysqli_connect_error());
-	     }       
+	    }       
 
 	    $sql_tble = "assignment_one";
 
-            $sql = "SELECT * FROM ".$sql_tble."";
+        $sql = "SELECT * FROM ".$sql_tble."";
             
 	    // Executes the query to check the table exists:
 	    $result = mysqli_query($conn, $sql);
-            if(!$result){
-                echo 'No status found in the system. Please go to the post status page to post one. <br>';
-                echo '<a href=poststatusform.php>Post a new Status<a> <br>';
-		echo '<a href=index.html>Return to Home Page<a>';
-                return;
-            }
+        if(!$result){
+            echo 'No status found in the system. Please go to the post status page to post one. <br>';
+            echo '<a href=poststatusform.php>Post a new Status<a> <br>';
+			echo '<a href=index.html>Return to Home Page<a>';
+            return;
+        }
 
 	    $sql = "SELECT * FROM ".$sql_tble." WHERE status_description LIKE '%".$search."%'";
 	    
@@ -41,41 +41,41 @@
 	    $result = mysqli_query($conn, $sql);
 	    
 	    if(mysqli_num_rows($result) == 0){
-                echo 'Status not found. Please try a different keyword. <br> <br>';
-                echo '<a href=searchstatusform.html>Search for another status<a> <br>';
-		echo '<a href=index.html>Return to Home Page<a>';
-                return;
-            } else {
+            echo 'Status not found. Please try a different keyword. <br> <br>';
+            echo '<a href=searchstatusform.html>Search for another status<a> <br>';
+			echo '<a href=index.html>Return to Home Page<a>';
+            return;
+        } else {
     		while($row = mysqli_fetch_assoc($result)) {
 
-			if($row['allow_like'] == 1){
-				$allowLike = "Allow Like";
-			} else {
-				$allowLike = "";
-			}
+				if($row['allow_like'] == 1){
+					$allowLike = "Allow Like";
+				} else {
+					$allowLike = "";
+				}
 
-			if($row['allow_comment'] == 1){
-				$allowComment = "Allow Comment";
-			} else {
-				$allowComment = "";
-			}
+				if($row['allow_comment'] == 1){
+					$allowComment = "Allow Comment";
+				} else {
+					$allowComment = "";
+				}
 
-			if($row['allow_share'] == 1){
-				$allowShare = "Allow Share";
-			} else {
-				$allowShare = "";
-			}
+				if($row['allow_share'] == 1){
+					$allowShare = "Allow Share";
+				} else {
+					$allowShare = "";
+				}
 
         		// Display the details of the matched status record
-			echo '<div style="border: 1px solid black; padding: 10px;">';
-			echo 'Status: '.$row['status_description'].'<br>';
+				echo '<div style="border: 1px solid black; padding: 10px;">';
+				echo 'Status: '.$row['status_description'].'<br>';
         		echo 'Status Code: '.$row['status_code'].'<br> <br>';
-			echo 'Share: '.$row['share'].'<br>';
-			echo 'Date Posted: '.$row['status_date'].'<br>';
-			echo 'Permission: '. ($allowLike ? $allowLike . " | " : '') . ($allowComment ? $allowComment  . " | " : '') . ($allowShare ? $allowShare : '') .'<br>';
-			echo '</div>';
-			echo '<br>';
-    	    		}
+				echo 'Share: '.$row['share'].'<br>';
+				echo 'Date Posted: '.$row['status_date'].'<br>';
+				echo 'Permission: '. ($allowLike ? $allowLike . " | " : '') . ($allowComment ? $allowComment  . " | " : '') . ($allowShare ? $allowShare : '') .'<br>';
+				echo '</div>';
+				echo '<br>';
+    	    }
 		}
         } else {
             echo 'The search string is empty. Please enter a keyword to search.';
